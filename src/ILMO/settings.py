82fd4f33ce417efb6debcac7071897924e9f088c
Julian-Samuel Gebühr <julian-samuel@gebuehr.net>
2021-09-13 14:11:50 +0200

    Make reproducible setup by adding .env

--- src/ILMO/settings.py
+++ src/ILMO/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=cary&s#0xsv=f7y&jqu^l%ro-kqgen$c$e+-)yz*r)t07=udh'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG")
 
 ALLOWED_HOSTS = []
 
@@ -76,11 +77,13 @@ WSGI_APPLICATION = 'ILMO.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': config("DATABASE_NAME"),
+        'USER': config("DATABASE_USER"),
+        'PASSWORD': config("DATABASE_PASSWORD"),
+        'HOST': config("DATABASE_HOST"),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
---
+++ src/README.md
@@ -0,0 +1,5 @@
+# Prequesits
+
+* Install python and pip
+* Install and configure database (examples with mysql)
+* Install dependencies `sudo apt-get install python3-dev default-libmysqlclient-dev build-essential`
