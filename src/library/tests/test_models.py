5ea86fe4787376786d0abab1191804c56f023f92
Julian-Samuel Gebühr <julian-samuel@gebuehr.net>
2021-09-14 19:35:36 +0200

    Add test for genre

--- src/library/tests/test_models.py
+++ src/library/tests/test_models.py
@@ -33,11 +33,37 @@ class AuthorModelTest(TestCase):
         ordering = author._meta.ordering
         self.assertEquals(ordering, ['last_name', 'first_name'] )
 
+class GenreModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(ctl):
+        Genre.objects.create(name="Science Fiction")
+        Book.objects.create(title="How to Test genres",
+                author=Author.objects.create(first_name="Jane", last_name="Doe"),
+                summary="Book to test genres",
+                isbn="1234567890124")
+
+    def test_str(self):
+        genre = Genre.objects.get(id=1)
+        string_representation = str(genre)
+        self.assertEquals(string_representation, f"Science Fiction")
+
+    def add_genre_to_book(self):
+        book = Book.objects.get(id=1)
+        genre = Genre.objects.get(id=1)
+        book.genre.set(genre)
+        string_representation = str(book.genre)
+        self.assertEquals(string_representation, f"Science Fiction")
+
+
+
+
+
 class BookModelTest(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        b = Book.objects.create(title="How to Test",
+        Book.objects.create(title="How to Test",
                 author=Author.objects.create(first_name="Jane", last_name="Doe"),
                 summary="How to write better tests than you do",
                 isbn="1234567890123")
