8f442fbdbcfc04546815919a6e7ea244125ef0e7
Julian-Samuel Gebühr <julian-samuel@gebuehr.net>
2021-09-14 04:53:53 +0200

    Use .env only for not travis

--- src/ILMO/settings.py
+++ src/ILMO/settings.py
@@ -20,11 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG")
 
 ALLOWED_HOSTS = []
 
@@ -92,6 +87,8 @@ if os.getenv('BUILD_ON_TRAVIS', None):
         }
     }
 else:
+    SECRET_KEY = config("SECRET_KEY")
+    DEBUG = config("DEBUG")
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.mysql', 
