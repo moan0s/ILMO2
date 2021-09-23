f5619e39ea608e39de3075016d3c722e82e0d5f7
moanos <julian-samuel@gebuehr.net>
2021-09-24 00:21:04 +0200

    Redirect to root, not profile

--- src/ILMO/settings.py
+++ src/ILMO/settings.py
@@ -153,3 +153,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Redirect to home URL after login (Default redirects to /accounts/profile/)
+LOGIN_REDIRECT_URL = '/'
