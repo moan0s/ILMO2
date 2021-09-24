5f3afae1b50e345880c496a315a6930e22df5631
moanos <julian-samuel@gebuehr.net>
2021-09-24 09:11:21 +0200

    Improve admin representation of book instance

--- src/library/admin.py
+++ src/library/admin.py
@@ -3,7 +3,19 @@ from .models import Book, Loan, BookInstance, Genre, Author
 
 admin.site.register(Book)
 admin.site.register(Loan)
-admin.site.register(BookInstance)
 admin.site.register(Genre)
 admin.site.register(Author)
 
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('label', 'book', 'status', 'borrower', 'due_back')
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book','label', 'imprint')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back','borrower')
+        }),
+    )
