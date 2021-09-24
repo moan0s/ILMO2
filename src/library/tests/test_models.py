bc93cc3bf405cb58f67d7eb2e6ef8cb84f6881c5
moanos <julian-samuel@gebuehr.net>
2021-09-24 10:37:38 +0200

    More bookInstance tests, possible fix of #3

--- src/library/tests/test_models.py
+++ src/library/tests/test_models.py
@@ -33,6 +33,30 @@ class AuthorModelTest(TestCase):
         author = Author.objects.get(id=1)
         ordering = author._meta.ordering
         self.assertEquals(ordering, ['last_name', 'first_name'] )
+    def test_first_name_label(self):
+        author = Author.objects.get(id=1)
+        field_label = author._meta.get_field('first_name').verbose_name
+        self.assertEqual(field_label, 'first name')
+
+    def test_date_of_death_label(self):
+        author = Author.objects.get(id=1)
+        field_label = author._meta.get_field('date_of_death').verbose_name
+        self.assertEqual(field_label, 'Died')
+
+    def test_first_name_max_length(self):
+        author = Author.objects.get(id=1)
+        max_length = author._meta.get_field('first_name').max_length
+        self.assertEqual(max_length, 100)
+
+    def test_object_name_is_last_name_comma_first_name(self):
+        author = Author.objects.get(id=1)
+        expected_object_name = f'{author.last_name}, {author.first_name}'
+        self.assertEqual(str(author), expected_object_name)
+
+    def test_get_absolute_url(self):
+        author = Author.objects.get(id=1)
+        # This will also fail if the urlconf is not defined.
+        self.assertEqual(author.get_absolute_url(), '/library/author/1/')
 
 class GenreModelTest(TestCase):
 
@@ -82,30 +106,34 @@ class BookInstanceModelTest(TestCase):
                 author=Author.objects.create(first_name="Jane", last_name="Doe"),
                 summary="How to write better tests than you do",
                 isbn="1234567890123")
-        BookInstance.objects.create(book = b, label = "T 1 a")
+        bookInstanceA = BookInstance.objects.create(book = b, label = "T 1 a")
+        b.save()
+        bookInstanceA.save()
         
         u = User.objects.create_user('foo', password='bar')
-        BookInstance.objects.create(book = b,
-        label = "T 1 b",
-        borrower = u,
-        due_back = date.today() - timedelta(days=1))
-        BookInstance.objects.create(book = b,
-        label = "T 1 c",
-        borrower = u,
-        due_back = date.today() + timedelta(days=1))
-        
+        bookInstanceB = BookInstance.objects.create(book = b,
+            label = "T 1 b",
+            borrower = u,
+            due_back = date.today() - timedelta(days=1))
+
+        bookInstanceC = BookInstance.objects.create(book = b,
+            label = "T 1 c",
+            borrower = u,
+            due_back = date.today() + timedelta(days=1))
+        bookInstanceB.save()
+        bookInstanceC.save()
 
     def test_str(self):
-        bookInstance = BookInstance.objects.get(id=1)
+        bookInstance = BookInstance.objects.all()[0]
         string_representation = str(bookInstance)
         self.assertEquals(string_representation, f"[T 1 a] {bookInstance.book.title} by {bookInstance.book.author}")
 
     def test_default(self):
-        bookInstance = BookInstance.objects.get(id=1)
+        bookInstance = BookInstance.objects.all()[0]
         self.assertEquals(bookInstance.status, "m")
     
     def test_due_date(self):
-        bookInstanceB = BookInstance.objects.get(id=2)
+        bookInstanceB = BookInstance.objects.all()[1]
         self.assertEquals(True, bookInstanceB.is_overdue)
-        bookInstanceC = BookInstance.objects.get(id=3)
+        bookInstanceC = BookInstance.objects.all()[2]
         self.assertEquals(False, bookInstanceC.is_overdue)
