2f46c00e82679aea67973ed78c13911d19ff6fb2
moanos <julian-samuel@gebuehr.net>
2021-09-26 01:45:10 +0200

    Add language

--- src/library/models.py
+++ src/library/models.py
@@ -24,6 +24,7 @@ class Book(models.Model):
     genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
     summary = models.TextField(max_length=1000, help_text='Enter a brief description of the book')
     isbn = models.CharField('ISBN', max_length=13, unique=True, help_text='ISBN number (13 Characters)')
+    language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
 
 class BookInstance(models.Model):
     """Represents a copy of a book that is physically in the library"""
@@ -84,3 +85,12 @@ class Author(models.Model):
     def get_absolute_url(self):
         """Returns the url to access a detail record for this book."""
         return reverse('library:author-detail', args=[str(self.id)])
+    
+class Language(models.Model):
+    """Model representing a Language (e.g. English, French, Japanese, etc.)"""
+    name = models.CharField(max_length=200,
+                            help_text="Enter the book's natural language (e.g. English, French, Japanese etc.)")
+
+    def __str__(self):
+        """String for representing the Model object (in Admin site etc.)"""
+        return self.name
