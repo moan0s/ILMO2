79a2184491a2ca2a2a24086a0ad3e6561b9e54db
moanos <julian-samuel@gebuehr.net>
2021-09-26 01:28:38 +0200

    Test author list view

--- src/library/tests/test_views.py
+++ src/library/tests/test_views.py
@@ -250,3 +250,43 @@ class AuthorViewTest(TestCase):
         # Check that a author without books is correctly displayed
         self.assertNotContains(response, "How to Test views")
         self.assertContains(response, "No books by this author.")
+
+class AuthorListViewTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Create 13 authors for pagination tests
+        number_of_authors = 13
+
+        for author_id in range(number_of_authors):
+            Author.objects.create(
+                first_name=f'Christian {author_id}',
+                last_name=f'Surname {author_id}',
+            )
+
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/library/authors/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('library:authors'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('library:authors'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'library/authors.html')
+
+    def test_pagination_is_ten(self):
+        response = self.client.get(reverse('library:authors'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('is_paginated' in response.context)
+        self.assertTrue(response.context['is_paginated'] == True)
+        self.assertEqual(len(response.context['author_list']), 10)
+
+    def test_lists_all_authors(self):
+        # Get second page and confirm it has (exactly) remaining 3 items
+        response = self.client.get(reverse('library:authors')+'?page=2')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('is_paginated' in response.context)
+        self.assertTrue(response.context['is_paginated'] == True)
+        self.assertEqual(len(response.context['author_list']), 3)
