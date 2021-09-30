e25692a3e094d76a0d70f921d42f0e7356229b3b
moanos <julian-samuel@gebuehr.net>
2021-09-30 13:36:52 +0200

    Add create link to openingohour view

--- src/library/urls.py
+++ src/library/urls.py
@@ -53,7 +53,7 @@ urlpatterns += [
 
 urlpatterns += [
     # ex: /library/openinghour/create
-    path('openinghour/create', views.OpeningHoursCreateView.as_view(), name='create-openinghour'),
+    path('openinghour/create', views.OpeningHoursCreateView.as_view(), name='openinghour-create'),
     # ex: /library/<int:pk>/delete
     path('openinghour/<int:pk>/delete', views.OpeningHourDeleteView.as_view(), name='openinghour-delete'),
     # ex: /library/openinghours/
