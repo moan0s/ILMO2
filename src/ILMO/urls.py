f7f6c32d6f2eefff8b70de45c1d472be84dfeb19
moanos <julian-samuel@gebuehr.net>
2021-10-13 21:13:53 +0200

    Add admin navbar

--- src/ILMO/urls.py
+++ src/ILMO/urls.py
@@ -5,7 +5,7 @@ from django.views.generic import RedirectView
 urlpatterns = [
     path('', RedirectView.as_view(url='library/', permanent=True)),
     path('library/', include('library.urls')),
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name="admin"),
     path("accounts/", include("django.contrib.auth.urls")),
 ]
 
