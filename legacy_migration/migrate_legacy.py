a1e59197c1d87dc20a224b806589c2a37c2dd63c
Julian-Samuel Gebühr <julian-samuel@gebuehr.net>
2021-11-09 07:54:13 +0100

    Add User creation

--- legacy_migration/migrate_legacy.py
+++ legacy_migration/migrate_legacy.py
@@ -1,29 +1,38 @@
-# from library.models import Book, BookInstance, Material, MaterialInstance, User
-# from django.contrib.auth.models import User
-import json
+from library.models import Book, BookInstance, Material, MaterialInstance, Member, Language
+from django.contrib.auth.models import User
 
-with open('user.json') as json_file:
+import json
+import os
+
+with open('../legacy_migration/user.json') as json_file:
     data = json.load(json_file)
     user_list = data[2]["data"]
     print(f"Loaded {len(user_list)} user.")
 
-with open('books.json') as json_file:
+with open('../legacy_migration/books.json') as json_file:
     data = json.load(json_file)
     old_bookinstance_list = data[2]["data"]
     print(f"Loaded {len(old_bookinstance_list)} book instances.")
 
-with open('material.json') as json_file:
+with open('../legacy_migration/material.json') as json_file:
     data = json.load(json_file)
     old_materialinstance_list = data[2]["data"]
     print(f"Loaded {len(old_materialinstance_list)} material instances.")
 
-with open('loan.json') as json_file:
+with open('../legacy_migration/loan.json') as json_file:
     data = json.load(json_file)
     old_loan_list = data[2]["data"]
     print(f"Loaded {len(old_loan_list)} loans.")
 
-for user in user_list:
-    pass
+for legacy_user in user_list:
+    user = User.objects.create(first_name=legacy_user["forename"],
+                               last_name=legacy_user["forename"],
+                               email=legacy_user["email"],
+                               username=legacy_user["user_ID"])
+    member = Member.objects.get(user=user)
+    print(legacy_user['language'])
+    member.preferred_language = Language.objects.get(name=legacy_user['language'])
+    member.UID = legacy_user["UID"]
 
 
 def get_stem(book_ID):
@@ -42,7 +51,7 @@ for test in test_cases:
         stem = get_stem(test[0])
         assert stem == test[1]
     except AssertionError:
-        #print(f"Calculated: {stem}, Expected {test[1]}")
+        print(f"Calculated: {stem}, Expected {test[1]}")
         raise AssertionError
 
 books = dict()
@@ -51,7 +60,7 @@ for old_book_instance in old_bookinstance_list:
     try:
         books[label_stem]["number"] += 1
     except KeyError:
-        #print(f"Creating new book: {label_stem}")
+        # print(f"Creating new book: {label_stem}")
         books[label_stem] = {"title": old_book_instance["title"],
                              "number": 1}
 print(f"Derived {len(books)} books")
@@ -62,7 +71,7 @@ for old_material_instance in old_materialinstance_list:
     try:
         material[label_stem]["number"] += 1
     except KeyError:
-        #print(f"Creating new material: {label_stem}")
+        # print(f"Creating new material: {label_stem}")
         material[label_stem] = {"title": old_material_instance["name"],
                                 "number": 1}
 print(f"Derived {len(material)} materials")
