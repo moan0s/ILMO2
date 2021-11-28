173001820a8f7387aa983959016f8688182b5bd7
Julian-Samuel Gebühr <julian-samuel@gebuehr.net>
2021-11-28 12:14:42 +0100

    Allow all hosts

--- src/ilmo/settings.py
+++ src/ilmo/settings.py
@@ -37,9 +37,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
