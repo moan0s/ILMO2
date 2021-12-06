d0cfd8c8df9816470560ff54cfda8db3b8321352
Julian-Samuel Gebühr <julian-samuel@gebuehr.net>
2021-12-06 08:58:09 +0100

    Add static root to settings and add instructions

--- src/ilmo/settings.py
+++ src/ilmo/settings.py
@@ -33,6 +33,8 @@ DB_HOST = config.get("database", "host", fallback="localhost")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+""" URLS """
+STATIC_ROOT = config.get("locations", "static", fallback=".django-static/")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
