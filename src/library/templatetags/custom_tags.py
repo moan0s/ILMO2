e281584fae8dd3c8cdb4ed36a1d376d7a7972553
Julian-Samuel Gebühr <julian-samuel@gebuehr.net>
2021-12-09 18:08:41 +0100

    Show authors correctly, clarify safeness of join

--- src/library/templatetags/custom_tags.py
+++ src/library/templatetags/custom_tags.py
@@ -5,6 +5,13 @@ register = template.Library()
 
 @register.filter('join_link')
 def join_link(value, arg):
+    """
+    Joins the items of value as link to itself together using arg
+
+    The item in values are properly escaped so the safe filter can be applied
+    Example usage:
+    {%  book.author.all|join_link:", " % | safe}
+    """
     from django.utils.html import conditional_escape
     arr = []
     for i in value:
