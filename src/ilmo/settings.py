02bd6f4b9595553925cbab24aa14ff36c457a8ad
Sebastian <mail@sebastian-ludwig.name>
2022-01-03 11:39:19 +0100

    Enable translation via URL
    switch translation via root/[language_code]/

--- src/ilmo/settings.py
+++ src/ilmo/settings.py
@@ -83,6 +83,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
 ]
 
 ROOT_URLCONF = 'ilmo.urls'
@@ -165,7 +166,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'de'
+LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'CET'
 
