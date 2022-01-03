31fc9c86420610305993ff5131e6a4a4e39b2a3f
Sebastian <mail@sebastian-ludwig.name>
2022-01-03 12:08:36 +0100

    Add language switcher in header file

--- src/ilmo/views.py
+++ src/ilmo/views.py
@@ -15,3 +15,12 @@ def security_txt(request):
              "Policy: " + ilmo.settings.SEC_POLICY
              ]
     return HttpResponse("\n".join(lines), content_type="text/plain")
+class HomePageView(TemplateView):
+
+    def render_to_response(self, context, **response_kwargs):
+        from django.utils import translation
+        user_language = 'en-us'
+        translation.activate(user_language)
+        response = super(HomePageView, self).render_to_response(context, **response_kwargs)
+        response.set_cookie(ilmo.settings.LANGUAGE_COOKIE_NAME, user_language)
+        return response
