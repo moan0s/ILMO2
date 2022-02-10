51244c387b2a4f5d21579817e011c58beae77331
Sebastian15 <mail@sebastian-ludwig.name>
2022-02-10 13:41:04 +0100

    Basic registration with standard views

--- src/ilmo/urls.py
+++ src/ilmo/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import RedirectView
 from django.conf.urls.i18n import i18n_patterns
+from . import views
 from .views import *
 
 urlpatterns = i18n_patterns(
