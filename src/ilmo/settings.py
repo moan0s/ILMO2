88f3280825f394e345c0e24ae50a5a0f597006b1
Sebastian15 <mail@sebastian-ludwig.name>
2022-02-10 10:37:32 +0100

    Basic configuration and templates

--- src/ilmo/settings.py
+++ src/ilmo/settings.py
@@ -201,3 +201,5 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     ]
 }
+
+ACCOUNT_ACTIVATION_DAYS = 7 # One-week activation window
--- src/ilmo/urls.py
+++ src/ilmo/urls.py
@@ -8,6 +8,7 @@ urlpatterns = i18n_patterns(
     path('', RedirectView.as_view(url='library/', permanent=True)),
     path('library/', include('library.urls')),
     path('admin/', admin.site.urls, name="admin"),
+    path('accounts/', include('django_registration.backends.activation.urls')),
     path("accounts/", include("django.contrib.auth.urls")),
     path(".well-known/security.txt", security_txt),
     path("api-auth/", include('rest_framework.urls')),
