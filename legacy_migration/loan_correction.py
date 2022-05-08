e7795d0708f45c0189553562fd00e93c7d2770b3
Julian-Samuel Gebühr <julian-samuel@gebuehr.net>
2022-05-08 17:14:35 +0200

    fix loan returns

--- legacy_migration/loan_correction.py
+++ legacy_migration/loan_correction.py
@@ -12,33 +12,21 @@ with open('../legacy_migration/loan.json') as json_file:
     old_loan_list = data[2]["data"]
     print(f"Loaded {len(old_loan_list)} loans.")
 
+print(old_loan_list)
 unhandled_loans = []
 for legacy_loan in old_loan_list:
-    print(f"Loan {legacy_loan}")
+    new_id = int(legacy_loan["loan_ID"])-1
     try:
-        if legacy_loan["type"] == "book":
-            try:
-                item = BookInstance.objects.get(label=legacy_loan["ID"])
-            except BookInstance.DoesNotExist:
-                item = BookInstance.objects.get(label=legacy_loan["ID"]+" a")
-        else:
-            try:
-                item = MaterialInstance.objects.get(label=legacy_loan["ID"])
-            except MaterialInstance.DoesNotExist:
-                item = BookInstance.objects.get(label=legacy_loan["ID"]+" a")
-
-        loan = Loan.objects.filter(item=item, lent_on=legacy_loan["pickup_date"])
-        return_date = legacy_loan["return_date"]
-        """
-        if return_date != "0000-00-00":
-            return_date_as_dt = datetime.strptime(return_date, "%Y-%m-%d").date()
-            item.return_item()
-        last_reminder = legacy_loan["last_reminder"]
-        loan.save()
-        """
-        print(loan)
-    except Exception as e:
-        print(e)
+        loan = Loan.objects.get(pk=new_id)
         unhandled_loans.append(legacy_loan)
-    continue
-print(f"Could not handle the following loans {unhandled_loans}")
+    except Loan.DoesNotExist:
+        continue
+    return_date = legacy_loan["return_date"]
+    if return_date != "0000-00-00":
+        return_date_as_dt = datetime.strptime(return_date, "%Y-%m-%d").date()
+        loan.return_loan(return_date_as_dt)
+        print(f"Returned {loan}")
+    loan.save()
+
+print(unhandled_loans)
+print(len(unhandled_loans))
