7c57d00d16661ab4769ede0871194bb0e0acf793
Julian-Samuel Gebühr <julian-samuel@gebuehr.net>
2023-06-10 12:16:28 +0200

    Add git entrypoint

---
+++ docker/ilmo.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -eux
+
+cd /var/ilmo/src
+export DATA_DIR=/var/ilmo/
+source /var/ilmo/venv/bin/activate
+
+AUTOMIGRATE=${AUTOMIGRATE:-yes}
+NUM_WORKERS_DEFAULT=$((2 * $(nproc --all)))
+export NUM_WORKERS=${NUM_WORKERS:-$NUM_WORKERS_DEFAULT}
+
+ilmo-manage collectstatic --noinput
+
+if [ "$AUTOMIGRATE" != "skip" ]; then
+  ilmo-manage migrate --noinput
+fi
+
+exec gunicorn ilmo.wsgi \
+    --name ilmo \
+    --workers $NUM_WORKERS \
+    --max-requests 1200 \
+    --max-requests-jitter 50 \
+    --log-level=info \
+    --bind 0.0.0.0:8345
+
